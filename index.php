7d9b35478fab2a3766b6e72a731a14fb513ab49f
Enrique Koyoc <kikekc10@gmail.com>
2021-02-06 02:52:30 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <label>Selecciona una imagen:</label>
+    <input type="text" name="nombre"><br>
+        <input type="text" name="descripcion"><br>
+
+    <input type="file" name="image"><br>
+    <input type="submit" name="submit" value="Subir">
+</form>
---
+++ upload.php
@@ -0,0 +1,84 @@
+<?php 
+ 
+    require "conexion.php";
+    $nombre_accesorio=$_POST['nombre'];
+    $descripcion=$_POST['descripcion'];
+
+  
+/* 
+ * Función personalizada para comprimir y 
+ * subir una imagen mediante PHP
+ */ 
+function compressImage($source, $destination, $quality) { 
+    // Obtenemos la información de la imagen
+    $imgInfo = getimagesize($source); 
+    $mime = $imgInfo['mime']; 
+     
+    // Creamos una imagen
+    switch($mime){ 
+        case 'image/jpeg': 
+            $image = imagecreatefromjpeg($source); 
+            break; 
+        case 'image/png': 
+            $image = imagecreatefrompng($source); 
+            break; 
+        case 'image/gif': 
+            $image = imagecreatefromgif($source); 
+            break; 
+        default: 
+            $image = imagecreatefromjpeg($source); 
+    } 
+     
+    // Guardamos la imagen
+    imagejpeg($image, $destination, $quality); 
+     
+    // Devolvemos la imagen comprimida
+    return $destination; 
+} 
+ 
+ 
+// Ruta subida
+$uploadPath = "uploads/"; 
+ 
+// Si el fichero se ha enviado
+$status = $statusMsg = ''; 
+if(isset($_POST["submit"])){ 
+    $status = 'error'; 
+    if(!empty($_FILES["image"]["name"])) { 
+        // File info 
+        $fileName = basename($_FILES["image"]["name"]); 
+        $imageUploadPath = $uploadPath . $fileName; 
+        $fileType = pathinfo($imageUploadPath, PATHINFO_EXTENSION); 
+         
+        // Permitimos solo unas extensiones
+        $allowTypes = array('jpg','png','jpeg','gif'); 
+        if(in_array($fileType, $allowTypes)){ 
+            // Image temp source 
+            $imageTemp = $_FILES["image"]["tmp_name"]; 
+             
+            // Comprimos el fichero
+            $compressedImage = compressImage($imageTemp, $imageUploadPath, 75); 
+             
+            if($compressedImage){ 
+                $status = 'success'; 
+                $statusMsg = "La imagen se ha subido satisfactoriamente."; 
+            }else{ 
+                $statusMsg = "La compresion de la imagen ha fallado"; 
+            } 
+        }else{ 
+            $statusMsg = 'Lo sentimos, solo se permiten imágenes con estas extensiones: JPG, JPEG, PNG, & GIF.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Por favor, selecciona una imagen.'; 
+    } 
+} 
+ 
+ $ruta = $uploadPath.$fileName;
+// Mostrar el estado de la imagen 
+echo $statusMsg; 
+
+ $queryAccesorio="INSERT INTO accesorios(id_accesorio,nombre_accesorio,descripcion,ruta_imagen)
+VALUES(NULL, '$nombre_accesorio', '$descripcion',' $ruta')";
+    echo mysqli_query($conexion,$queryAccesorio)
+ 
+?>
